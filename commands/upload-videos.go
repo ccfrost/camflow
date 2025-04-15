d224dd9afa25f14a4043171c29891fc95dd81677
Chris Frost <chris@frostnet.net>
2025-04-15 14:15:16 -0700

    add empty UploadVideos command

--- commands/upload-videos.go
+++ commands/upload-videos.go
@@ -1,1 +1,8 @@
 package commands
+
+import "github.com/ccfrost/camedia/camediaconfig"
+
+// UploadVideos uploads videos from the staging video dir to Google Photos.
+func UploadVideos(config camediaconfig.CamediaConfig) error {
+	return nil
+}
