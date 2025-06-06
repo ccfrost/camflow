df2434e83c511ad008050dde77eb91a5b66631d7
Chris Frost <chris@frostnet.net>
2025-06-06 11:35:33 -0700

    nit some log code

--- commands/logger.go
+++ commands/logger.go
@@ -8,21 +8,13 @@ import (
 var logger *slog.Logger
 
 func init() {
-	// Check for debug/verbose environment variables
-	debugMode := os.Getenv("DEBUG") != "" || os.Getenv("VERBOSE") != ""
-
-	var level slog.Level
-	if debugMode {
+	level := slog.LevelInfo
+	if os.Getenv("DEBUG") != "" {
 		level = slog.LevelDebug
-	} else {
-		level = slog.LevelInfo
 	}
 
-	// Create handler options
 	opts := &slog.HandlerOptions{
 		Level: level,
 	}
-
-	// Use TextHandler for human-readable output
 	logger = slog.New(slog.NewTextHandler(os.Stderr, opts))
 }
