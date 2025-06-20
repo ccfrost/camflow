db6dc493ab9c9681d340195ed106b7bf67f5bbbc
Chris Frost <chris@frostnet.net>
2025-06-20 13:09:52 -0700

    update func doc

--- commands/upload_videos.go
+++ commands/upload_videos.go
@@ -7,11 +7,10 @@ import (
 	"github.com/ccfrost/camflow/camflowconfig"
 )
 
-// UploadVideos uploads videos from the video export queue dir to Google Photos.
-// Videos are added to all albums in config.DefaultAlbums.
-// Uploaded videos are moved from export queue to VideosExportedRoot unless keepQueued is true.
+// UploadVideos uploads videos from the video export queue to Google Photos.
+// Videos are added to Google Photos album named DefaultAlbum.
+// Uploaded videos are moved from export queue to exported dir; unless keepQueued is true, in which case they are copied (but not moved).
 // The function is idempotent - if interrupted, it can be recalled to resume.
-// Takes configDir to locate token and cache files, and a gphotosClient for API interaction.
 func UploadVideos(ctx context.Context, config camflowconfig.CamflowConfig, cacheDirFlag string, keepQueued bool, gphotosClient GPhotosClient) error {
 	if err := config.Validate(); err != nil {
 		return fmt.Errorf("invalid config: %w", err)
